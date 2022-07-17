d07a8aaac415a155699f7edcca4f1b88c9a3b4bf
kazukiokumura <kazuki1025okumura@gmail.com>
2022-07-17 19:43:45 +0900

    Add: 管理画面のビューを作成 #23

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
 
-  get 'login' => 'user_sessions#new', :as => :login
-  post 'login' => "user_sessions#create"
-  delete 'logout' => 'user_sessions#destroy', :as => :logout
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: "user_sessions#create"
+  delete 'logout', to: 'user_sessions#destroy'
 
   resources :users, only: %i[new create]
+
+  namespace :admin do
+    root to: 'dashboards#index'
+    get 'login', to: 'user_sessions#new'
+    post 'login', to: 'user_sessions#create'
+    delete 'logout', to: 'user_sessions#destroy'
+  end
 end
