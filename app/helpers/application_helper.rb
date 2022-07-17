d7325bab4cbd6d5bcc14cdebbdeed355311874b0
kazukiokumura <kazuki1025okumura@gmail.com>
2022-07-17 19:53:15 +0900

    Add: タイトルを動的に表示できるように設定 #23

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def page_title(page_title = '', admin = false)
+    base_title = if admin
+                  'FLUFF NAVI(管理画面)'
+                 else
+                  'FLUFF NAVI'
+                 end
+
+    page_title.empty? ? base_title : page_title + ' | ' + base_title
+  end
 end
